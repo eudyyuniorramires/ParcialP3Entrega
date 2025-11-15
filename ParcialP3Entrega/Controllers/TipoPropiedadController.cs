cfbcd31fc977d93474fcba9f3697c4f725defa0a
Omar-Sebastian28 <omarjoaquinminayasebastian@gmail.com>
2025-11-15 15:42:35 -0400

    GuardarTipoPropiedad en el controlador TipoPropiedad

--- ParcialP3Entrega/Controllers/TipoPropiedadController.cs
+++ ParcialP3Entrega/Controllers/TipoPropiedadController.cs
@@ -13,5 +13,17 @@ namespace ParcialP3Entrega.Controllers
         }
 
 
+        [HttpPost]
+        public JsonResult GuardarTipoPropiedad(TipoPropiedad oTP)
+        {
+            bool respuesta = false;
+
+            respuesta = (oTP.IdTipoPropiedad == 0)
+                ? TipoPropiedad.Instancia.Registrar(oTP)
+                : TipoPropiedad.Instancia.Modificar(oTP);
+            string mensaje = string.Empty;
+           
+            return Json(new { respuesta = respuesta}, JsonRequestBehavior.AllowGet);
+        }
     }
 }
