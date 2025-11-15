309de6808a329e42846f256c41de7ee6c73b0b3a
Omar-Sebastian28 <omarjoaquinminayasebastian@gmail.com>
2025-11-15 15:52:55 -0400

    Finalizacion Controller TipoPropiedad

--- ParcialP3Entrega/Controllers/TipoPropiedadController.cs
+++ ParcialP3Entrega/Controllers/TipoPropiedadController.cs
@@ -27,5 +27,33 @@ namespace ParcialP3Entrega.Controllers
            
             return Json(new { respuesta = respuesta}, JsonRequestBehavior.AllowGet);
         }
+
+        [HttPost]
+        public bool Eliminar(int id)
+        {
+            bool respuesta = true;
+            using (SqlConnection cxn = new SqlConnection(cn.db)) 
+            {
+                cxn.Open();
+                try 
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EliminarTipoPropiedad", cxn);
+                    cmd.parameters.AddWithValue("@IdTipoPropiedad", id);
+                    cmd.parameters.AddWithValue("Estatus", false);
+
+                    cmd.parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.commandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex) 
+                {
+                    respuesta = false;
+                }
+
+            }
+                respuesta = TipoPropiedad.Instancia.Eliminar(idTP);
+            return Json(new { respuesta = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
